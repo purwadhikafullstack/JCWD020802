168d44b97de21ee1d8259ef2c23572bd3f017466
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2023-12-13 16:08:39 +0700

    initial commit (#3)
    * Initial Setup (#1)
    
    * Initial Setup
    
    * Setup Prettier
    
    * Initial Setup (#2)
    
    * Initial Setup
    
    * Setup Prettier
    
    * Setup Tailwind Material
    
    * Modify Cypress
    
    ---------
    
    Co-authored-by: Ezra Ravin Mateus <48173571+ezraravinmateus@users.noreply.github.com>

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,17 @@
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: [
+    './index.html',
+    './src/**/*.{html,js}',
+    './src/pages/**/*.{html,js}',
+    './src/components/**/*.{html,js}',
+    "../../node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "../../node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
